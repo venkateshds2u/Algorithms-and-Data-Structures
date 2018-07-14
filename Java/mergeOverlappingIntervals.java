dc5225b38f9c8200a39a7ff0e2c6c922aca31312
venkateshds2u <venkatesh.ds2u@gmail.com>
2018-07-14 14:21:45 -0700

    Create mergeOverlappingIntervals.java

---
+++ Java/mergeOverlappingIntervals.java
@@ -0,0 +1,37 @@
+
+
+
+
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new IntervalComparator());
+        LinkedList<Interval> merged = new LinkedList<Interval>();
+        for(Interval interval: intervals)
+        {
+            if(merged.isEmpty()||merged.getLast().end<interval.start)
+                merged.add(interval);
+            else
+                merged.getLast().end=Math.max(merged.getLast().end, interval.end);
+        }
+        return merged;
+        
+    }
+    private class IntervalComparator implements Comparator<Interval>{
+        @Override
+        public int compare(Interval a, Interval b)
+        {
+            return a.start<b.start?-1:a.start==b.start?0:1;
+        }
+        
+    }
+}
